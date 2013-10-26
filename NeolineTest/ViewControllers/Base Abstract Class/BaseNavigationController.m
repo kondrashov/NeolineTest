5d5b600ba98b2b37412279df92818c7bbb4ab740
Artem Kondrashov <kondrashov.artem@gmail.com>
2013-10-26 16:07:02 +0300

    Add bar button items.

--- NeolineTest/ViewControllers/Base Abstract Class/BaseNavigationController.m
+++ NeolineTest/ViewControllers/Base Abstract Class/BaseNavigationController.m
@@ -9,6 +9,10 @@
 #import "BaseNavigationController.h"
 #import <QuartzCore/QuartzCore.h>
 
+#define BUTTON_FONT_SIZE          15
+#define BUTTON_TITLE_PADDING_X    10
+#define BUTTON_TITLE_PADDING_Y    5
+
 @interface BaseNavigationController ()
 
 @end
@@ -31,6 +35,8 @@
                                                   forBarMetrics:UIBarMetricsDefault];
     
     self.navigationItem.titleView = [self getTitleView];
+//    self.navigationItem.rightBarButtonItem = [self createBarButtonWithTitle:@"Готово" delegate:self selector:@selector(onRightBarButton:)];
+//    self.navigationItem.leftBarButtonItem = [self createBarButtonWithTitle:@"Назад" delegate:self selector:@selector(onLeftBarButton:)];
 }
 
 - (void)viewDidLoad
@@ -52,7 +58,33 @@
 
 - (UIBarButtonItem *)createBarButtonWithTitle:(NSString *)title delegate:(id)delegate selector:(SEL)selector
 {
-    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    CGSize btnTitleSize = [title sizeWithFont:[UIFont systemFontOfSize:BUTTON_FONT_SIZE]];
+    UIImage *btnImage = [[UIImage imageNamed:@"navBarButtonBackg"] safeResizableImageWithCapInsets:UIEdgeInsetsMake(3, 3, 3, 3)
+                                                                                      resizingMode:UIImageResizingModeStretch];
+    [button setBackgroundImage:btnImage forState:UIControlStateNormal];
+    [button setTitle:title forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [button.titleLabel setFont:[UIFont systemFontOfSize:BUTTON_FONT_SIZE]];
+    [button setFrame:CGRectMake(0, 0, btnTitleSize.width + BUTTON_TITLE_PADDING_X, btnTitleSize.height + BUTTON_TITLE_PADDING_Y)];
+    [button addTarget:delegate action:selector forControlEvents:UIControlEventTouchUpInside];
+    button.layer.borderColor = RGBCOLOR(146, 162, 172).CGColor;
+    button.layer.borderWidth = 1;
+    
+    UIBarButtonItem *barButtonItem = [[UIBarButtonItem alloc] initWithCustomView:button];
+    return [barButtonItem autorelease];
+}
+
+#pragma mark - Actions
+
+- (void)onRightBarButton:(id)sender
+{
+    // Override in childs
+}
+
+- (void)onLeftBarButton:(id)sender
+{
+    // Override in childs    
 }
 
 @end
